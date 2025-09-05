35530fa21a406e9e1c34896d4ff6d464c77e5c4f
Jasaman Singh Chaggar <jsc.21905@gmail.com>
2025-09-05 18:57:10 +0530

    fix: update sameSite attribute in cookieOptions based on NODE_ENV

--- backend/src/utils/cookieOptions.ts
+++ backend/src/utils/cookieOptions.ts
@@ -1,6 +1,6 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "lax" : "none",
   maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
 };
